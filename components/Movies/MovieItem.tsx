7b5d7995a905ec5352dfb419e745b1dab95cc5a1
Kenjy Vincent-Sully <contact@vincentsully.com>
2022-06-30 19:21:32 +0200

    feat(Top 10 Movies): Create Top Movies component

--- components/Movies/MovieItem.tsx
+++ components/Movies/MovieItem.tsx
@@ -1,12 +1,12 @@
 import { Card, Img } from "./style";
 const MovieItem = ({ movie }) => {
-    const { original_title, poster_path, release_date } = movie;
+
+    const { original_title, poster_path, release_date, title } = movie;
+
     return (
         <>
-            {/* <Image src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt="Coming soon" layout='responsive' width="50px" height="50px" /> */}
-            <Img src={`${process.env.NEXT_PUBLIC_PATH_IMG}${poster_path}`
-            } />
-            <p> {original_title}</p>
+            <Img src={`${process.env.NEXT_PUBLIC_PATH_IMG}${poster_path}`} alt={`${original_title} ` || ` ${title}`} />
+            <p>{title || original_title}</p>
             <p>{release_date}</p>
         </>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,37 @@
 import { InferProps } from "prop-types";
 import { GetStaticProps } from 'next';
-import { getTopMovies, getMoviesList } from "./api/movies";
-import { DataMoviePropsTypes } from "types/Movie";
+import { getTopMovies, getMoviesList, getFilteredMovies } from "./api/movies";
+import { DataMoviePropsTypes, GenresPropsTypes } from "types/Movie";
 import Layout from "layout";
 import MoviesTopList from "@components/Movies/MoviesTopList";
 import MoviesList from "@components/Movies/MoviesList";
+import MovieFilter from "@components/Movies/MovieFilter";
 
-export default function Movie({ initData }: InferProps<typeof Movie.propTypes>) {
+export default function Movie({ initData, filter }: InferProps<typeof Movie.propTypes>) {
 
     return (
         <Layout>
             <MoviesTopList data={initData} />
             <MoviesList />
+            <MovieFilter genres={filter} />
         </Layout>
     )
 }
 
 
-export const getStaticProps: GetStaticProps = async () => {
-    const data = await getTopMovies();
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const dataTopMovie = await getTopMovies();
+    const dataFilter = await getFilteredMovies();
 
     return {
         props: {
-            initData: data.results,
+            initData: dataTopMovie.results.slice(0, 10),
+            filter: dataFilter.genres,
         }
     }
 }
 
 Movie.propTypes = {
     initData: DataMoviePropsTypes,
+    filter: GenresPropsTypes,
 };
